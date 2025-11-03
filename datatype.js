47c571f9523af4274e57e91c2aa9455d940f77a4
Komal rajole <komaldhanwate2788@gmail.com>
2025-11-03 12:38:24 +0530

    hhgh

---
+++ datatype.js
@@ -0,0 +1,17 @@
+console.log("Datatypes in js");
+
+// Primitive datatypes in JS
+let name = "komal"; //string datatype
+let age = 30; //number datratypr
+let isLoggeIn = false; //boolean datatype
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggeIn);
+
+// Non-primitive datypes in JS
+let myArray = [1, 2, 3, 4, 5]; //array datatype
+let myObject = { car: "BMW", bike: "Pulsur" }; //object datatype
+
+console.log(typeof myArray);
+console.log(typeof myObject);
---
+++ variable.js
@@ -0,0 +1,7 @@
+const accountId = 1246535;
+let accountEmail = "komal@gmail.com";
+var accountPass = "12345";
+accountCity = "nashik";
+
+// accountId = 1456  Not aalowed
+console.log(accountCity);
