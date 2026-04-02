cdd5ac2830abbf588f672a1a42e2c4b04ffc9900
Komal rajole <komaldhanwate2788@gmail.com>
2026-04-02 09:58:33 +0530

    new code added

--- komal_Js-main/komal_Js-main/01_datatype_conversion.js
+++
@@ -1,8 +0,0 @@
-let score = false
-
-console.log(typeof score)
-console.log(typeof(score))
-
-let valueInnumber = Number(score)
-console.log(typeof valueInnumber)
-console.log(valueInnumber)
--- komal_Js-main/komal_Js-main/01_variables.js
+++
@@ -1,8 +0,0 @@
-const accountId = 12456
-let name = "komal rajole"
-var address = "nashik"
-// myCity - "pathardiphata"
-
-// accountId = 25689
-console.log(accountId);
-console.table([accountId,name,address])
